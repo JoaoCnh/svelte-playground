4c949392567b4e9eab79ad79f2708ecf60388bb6
JoaoCnh <joaomarinho.cunha@gmail.com>
2023-11-10 10:17:19 +0000

    Improve playgrounds page layout from 3 to 4 cols in >=lg breakpoint

--- src/routes/playground/+page.svelte
+++ src/routes/playground/+page.svelte
@@ -8,7 +8,7 @@
 
 <section id="Experiments" class="relative isolate px-6 pt-28 lg:px-8">
 	<ul
-		class="mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14"
+		class="mx-auto grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14"
 	>
 		{#each data.experiments as experiment (experiment.url)}
 			<li class="w-full h-full">
