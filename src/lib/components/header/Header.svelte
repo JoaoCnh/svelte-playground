c3e9fd6b80ffd03e5f87f9344ebf0b5b0477245f
JoaoCnh <joaomarinho.cunha@gmail.com>
2023-11-15 18:08:56 +0000

    Fix homepage icon not being clickable

--- src/lib/components/header/Header.svelte
+++ src/lib/components/header/Header.svelte
@@ -5,7 +5,7 @@
 <header class="absolute inset-x-0 top-0">
 	<nav class="flex items-center justify-center p-6 lg:px-8" aria-label="navigation">
 		<div class="flex">
-			<a href="/" class="-m-1.5 p-1.5" aria-label="homepage">
+			<a href="/" class="-m-1.5 p-1.5 z-40" aria-label="homepage">
 				<span class="sr-only">Svelte Playground</span>
 				<SvelteIcon class="h-8 w-auto text-orange-600" />
 			</a>
