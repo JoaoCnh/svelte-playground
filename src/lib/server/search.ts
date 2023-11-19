453aa3b4db03b486ed6b5a63b165d0bd79afec88
JoaoCnh <joaomarinho.cunha@gmail.com>
2023-11-19 22:08:47 +0000

    Fix vercel API route deployment that cannot read routes folders

--- src/lib/server/search.ts
+++ src/lib/server/search.ts
@@ -1,13 +1,24 @@
 import Fuse, { type FuseResult } from 'fuse.js';
-import { unslugify } from '$lib/unslugify';
-import { mapDir } from '$lib/server/map-dir';
 import type { SearchResult, SearchResultGroupItem } from '$lib/services/search/types';
 
-const experiments = mapDir('src/routes/playground', (dirName) => ({
-	icon: dirName,
-	label: unslugify(dirName),
-	url: `/playground/${dirName}`
-}));
+// We cannot use map dir here because Vercel deploys the serverless endpoint which won't have access to the routes folder
+// Let's imagine this is a fake search index
+const experiments = [
+	{ icon: null, label: 'actions', url: '/playground/actions' },
+	{ icon: 'apple', label: 'apple sequence', url: '/playground/apple-sequence' },
+	{ icon: null, label: 'aspect ratio', url: '/playground/aspect-ratio' },
+	{ icon: null, label: 'basic animations', url: '/playground/basic-animations' },
+	{ icon: null, label: 'carousel', url: '/playground/carousel' },
+	{ icon: null, label: 'context', url: '/playground/context' },
+	{ icon: null, label: 'editorial block', url: '/playground/editorial-block' },
+	{ icon: null, label: 'chart', url: '/playground/chart' },
+	{ icon: null, label: 'card link', url: '/playground/card-link' },
+	{ icon: null, label: 'polymorphism', url: '/playground/polymorphism' },
+	{ icon: null, label: 'slots', url: '/playground/slots' },
+	{ icon: null, label: 'stores', url: '/playground/stores' },
+	{ icon: null, label: 'toast', url: '/playground/toast' },
+	{ icon: null, label: 'transitions', url: '/playground/transitions' }
+];
 
 const playgrounds = [
 	{ icon: 'svelte', label: 'svelte playground', url: 'https://svelte-playground-iota.vercel.app/' },
@@ -21,7 +32,7 @@ const fuzzyPlaygrounds = new Fuse(playgrounds, fuzzyOptions);
 
 function transformFuseResult({
 	item
-}: FuseResult<{ icon: string; label: string; url: string }>): SearchResultGroupItem {
+}: FuseResult<{ icon: string | null; label: string; url: string }>): SearchResultGroupItem {
 	return {
 		icon: item.icon,
 		label: item.label,
