44363f87dfbd990df31573e29bcbedc913ff3a0a
JoaoCnh <joaomarinho.cunha@gmail.com>
2023-11-19 21:52:37 +0000

    Fix Spotlight on Safari

--- src/lib/components/spotlight/Spotlight.svelte
+++ src/lib/components/spotlight/Spotlight.svelte
@@ -36,23 +36,25 @@
 		on:close={close}
 		on:click|self={close}
 	>
-		<div
-			role="application"
-			class="h-full w-full max-w-[450px] flex flex-col overflow-hidden bg-neutral-500 rounded-lg border border-neutral-400 shadow-md"
-			use:trapFocus
-		>
-			<QueryForm on:query={(e) => (query = e.detail)} on:result={(e) => (results = e.detail)} />
+		<div>
+			<div
+				role="application"
+				class="h-full w-full max-w-[450px] flex flex-col overflow-hidden bg-neutral-500 rounded-lg border border-neutral-400 shadow-md"
+				use:trapFocus
+			>
+				<QueryForm on:query={(e) => (query = e.detail)} on:result={(e) => (results = e.detail)} />
 
-			{#if !!query}
-				<Results {results} />
-			{/if}
+				{#if !!query}
+					<Results {results} />
+				{/if}
+			</div>
+			<button
+				type="button"
+				class="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none"
+				on:click={close}
+			>
+				<CloseIcon class="w-4 h-4" />
+			</button>
 		</div>
-		<button
-			type="button"
-			class="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none"
-			on:click={close}
-		>
-			<CloseIcon class="w-4 h-4" />
-		</button>
 	</dialog>
 {/if}
