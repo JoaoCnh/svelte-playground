f3cf45610a327c4837a2f6eb92ed3a849b350fa6
JoaoCnh <joaomarinho.cunha@gmail.com>
2023-12-15 10:56:57 +0000

    Upgrade to Svelte.Kit 2 🚀

--- src/routes/playground/actions/+page.server.ts
+++ src/routes/playground/actions/+page.server.ts
@@ -54,7 +54,7 @@ export const actions = {
 		cookies.delete('userid', { path: '/' });
 		cookies.delete('sessionid', { path: '/' });
 		await db.deleteSession(sessionId || '');
-		throw redirect(308, '/playground/actions');
+		redirect(308, '/playground/actions');
 	},
 
 	create: async ({ cookies, request }) => {
@@ -62,21 +62,21 @@ export const actions = {
 		const data = await request.formData();
 
 		if (!sessionId) {
-			throw redirect(308, '/playground/actions');
-		}
+			redirect(308, '/playground/actions');
+		} else {
+			try {
+				const spell = data.get('spell') as string;
 
-		try {
-			const spell = data.get('spell') as string;
+				await db.createSpell(sessionId, spell);
 
-			await db.createSpell(sessionId, spell);
-
-			return { id: 'create', success: true };
-		} catch (error) {
-			return fail(500, {
-				id: 'create',
-				spell: data.get('spell'),
-				error: (error as Error).message
-			});
+				return { id: 'create', success: true };
+			} catch (error) {
+				return fail(500, {
+					id: 'create',
+					spell: data.get('spell'),
+					error: (error as Error).message
+				});
+			}
 		}
 	}
 } satisfies Actions;
